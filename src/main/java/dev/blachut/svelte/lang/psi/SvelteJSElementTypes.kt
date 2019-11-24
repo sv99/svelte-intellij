ca11d17829c9a03ee0e8d2034f72d7a37853ddca
Tomasz Błachut <blachut.tomasz@gmail.com>
2019-11-24 22:01:41 +0100

    Add SvelteJSElementTypes.EMBEDDED_CONTENT

--- src/main/java/dev/blachut/svelte/lang/psi/SvelteJSElementTypes.kt
+++ src/main/java/dev/blachut/svelte/lang/psi/SvelteJSElementTypes.kt
@@ -1,8 +1,10 @@
 package dev.blachut.svelte.lang.psi
 
 import com.intellij.lang.ASTNode
+import com.intellij.lang.javascript.types.JSEmbeddedContentElementType
 import com.intellij.lang.javascript.types.JSParameterElementType
 import com.intellij.psi.PsiElement
+import dev.blachut.svelte.lang.SvelteJSLanguage
 
 object SvelteJSElementTypes {
     val PARAMETER = object : JSParameterElementType("EMBEDDED_PARAMETER") {
@@ -12,5 +14,7 @@ object SvelteJSElementTypes {
     }
 
     val ATTRIBUTE_EXPRESSION = SvelteElementType("ATTRIBUTE_EXPRESSION")
+
+    val EMBEDDED_CONTENT = JSEmbeddedContentElementType(SvelteJSLanguage.INSTANCE, "SVELTE_")
 }
 
